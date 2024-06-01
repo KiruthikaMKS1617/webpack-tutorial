a7eab66324c74c936181c4fcfb1f3e9cea1bf732
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2024-06-01 17:31:20 +0530

    - image-caption accessed as nested micro frontend in kiwi

--- kiwi/webpack.dev.config.js
+++ kiwi/webpack.dev.config.js
@@ -70,6 +70,9 @@ module.exports = {
       exposes: {
         "./KiwiPage": "./src/components/kiwi-page/kiwi-page.js",
       },
+      remotes: {
+        ImageCaptionApp: "ImageCaptionApp@http://localhost:9003/remoteEntry.js",
+      },
     }),
   ],
 };
